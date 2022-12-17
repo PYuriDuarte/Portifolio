4a08a61a664f70cef39ea5dee94c378ea2ffae74
Yuri Duarte <116809586+PYuriDuarte@users.noreply.github.com>
2022-12-17 17:10:53 -0300

    commit

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,6 +6,15 @@ import pgod from '../videos/god.mp4';
 import plamp from '../videos/lamp.mp4';
 import pmusic from '../videos/music.mp4';
 import prank from '../videos/rank.mp4';
+import ppoke from '../videos/poke.mp4';
+
+import iimc from '../images/imc.png';
+import icep from '../images/cep.png';
+import igod from '../images/god.png';
+import ilamp from '../images/lamp.png';
+import imusic from '../images/music.png';
+import irank from '../images/rank.png';
+import ipoke from '../images/poke.png';
 
 function Projectsjs(){
 
@@ -15,32 +24,56 @@ function Projectsjs(){
       <div className='cardContainer'>
 
         <div className='cardContent'>
-          <video autoPlay muted>
-            <source src={pimc} type="video/mp4"/>
-          </video>
-        </div>
-        <div className='cardContent'>
-          <video autoPlay muted>
-            <source src={pcep} type="video/mp4"/>
-          </video>
-        </div>
-        <div className='cardContent'>
-          <video autoPlay muted>
-            <source src={prank} type="video/mp4"/>
-          </video>
-        </div>
-        <div className='cardContent'>
-          <video autoPlay muted>
-            <source src={pmusic} type="video/mp4"/>
-          </video>
-        </div>
-        <div className='cardContent'>
-          <video autoPlay muted>
+          <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
             <source src={pgod} type="video/mp4"/>
           </video>
         </div>
         <div className='cardContent'>
-          <video autoPlay muted>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={ppoke} type="video/mp4"/>
+          </video>
+        </div>
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={prank} type="video/mp4"/>
+          </video>
+        </div>
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={pimc} type="video/mp4"/>
+          </video>
+        </div>
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={pcep} type="video/mp4"/>
+          </video>
+        </div>        
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={pmusic} type="video/mp4"/>
+          </video>
+        </div>        
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={plamp} type="video/mp4"/>
+          </video>
+        </div>
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
+            <source src={plamp} type="video/mp4"/>
+          </video>
+        </div>
+        <div className='cardContent'>
+        <img src={igod} alt="site do god of war"/>
+          <video autoPlay muted loop>
             <source src={plamp} type="video/mp4"/>
           </video>
         </div>
