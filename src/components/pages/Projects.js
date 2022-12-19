ff1fd3c4aabe9b434a920c1164b069bd8aade7bb
Yuri Duarte <116809586+PYuriDuarte@users.noreply.github.com>
2022-12-19 12:00:36 -0300

    commit

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,7 +11,7 @@ import ppoke from '../videos/poke.mp4';
 
 import iimc from '../images/imc.png';
 import icep from '../images/cep.png';
-import igod from '../images/god.png';
+import igod from '../images/logogod.jpg';
 import ilamp from '../images/lamp.png';
 import imusic from '../images/music.png';
 import irank from '../images/rank.png';
@@ -26,40 +26,41 @@ function Projectsjs(){
       <div className='cardContainer'>
 
         <div className='cardContent'>
-          {/* <img src={igod} alt="site do god of war"/> */}
-          <ReactPlayer url="https://www.youtube.com/watch?v=wSjja0u0heg" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <div className='divgod'></div>
+          {/* <img src={igod} alt="site do god of war"/>
+          <ReactPlayer url="https://www.youtube.com/watch?v=wSjja0u0heg" className="video" onReady playIcon light width="32rem" height="18rem" muted loop playing/> */}
         </div>
         <div className='cardContent'>
           {/* <img src={ipoke} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/VeAH3mYyYzw" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/VeAH3mYyYzw" className="video" playIcon light width="32rem" height="18rem" muted loop playing/>
         </div>
         <div className='cardContent'>
           {/* <img src={irank} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/hsh_i75gIP0" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/hsh_i75gIP0" className="video" playIcon light width="32rem" height="18rem" muted loop playing/>
         </div>
         <div className='cardContent'>
           {/* <img src={iimc} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/ybtM0JouC9s" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/ybtM0JouC9s" className="video" playIcon light width="32rem" height="18rem" muted loop playing/>
         </div>
         <div className='cardContent'>
           {/* <img src={icep} alt="site do god of war"/> */}
-          <ReactPlayer url="https://www.youtube.com/watch?v=MAN3ZnxJE2o&t=4s" light className="video" width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://www.youtube.com/watch?v=MAN3ZnxJE2o&t=4s" playIcon light className="video" width="32rem" height="18rem" muted loop playing/>
         </div>        
         <div className='cardContent'>
           {/* <img src={imusic} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/LPq54FAlYuk" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/LPq54FAlYuk" className="video" playIcon light width="32rem" height="18rem" muted loop playing/>
         </div>        
         <div className='cardContent'>
           {/* <img src={ilamp} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/K5XC08kpang" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/K5XC08kpang" className="video" playIcon light width="32rem" height="18rem" muted loop playing/>
         </div>
         <div className='cardContent'>
           {/* <img src={ilamp} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/K5XC08kpang" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/K5XC08kpang" className="video" playIcon light width="32rem" height="18rem" muted loop playing/>
         </div>
         <div className='cardContent'>
           {/* <img src={ilamp} alt="site do god of war"/> */}
-          <ReactPlayer url="https://youtu.be/K5XC08kpang" className="video" light width="32rem" height="18rem" muted loop playing/>
+          <ReactPlayer url="https://youtu.be/K5XC08kpang" className="video" playIcon           light width="32rem" height="18rem" muted loop playing/>
         </div>
                
 
